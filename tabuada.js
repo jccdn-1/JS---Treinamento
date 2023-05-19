7a2f6174b6ef446ea95939ed413a90c7248c2b2e
Kimino <joaoif2020@hotmail.com>
2023-05-19 13:11:58 -0300

    Aprendendo a usar git

---
+++ tabuada.js
@@ -0,0 +1,9 @@
+let output = document.getElementById("output");
+
+function mostrarTabuada(numero) {
+    for (valor = 0; valor <=10; valor++) {
+
+        total = numero + valor + "<br>";
+
+    }
+}
